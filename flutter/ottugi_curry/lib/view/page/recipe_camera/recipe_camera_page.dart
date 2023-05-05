8509c6756d27c8f899bfa843cb729c17f50b59d4
Choi Yena <71956482+YenaChoi00@users.noreply.github.com>
2023-05-06 02:25:30 +0900

    Implement Result Check Page

--- flutter/ottugi_curry/lib/view/page/recipe_camera/recipe_camera_page.dart
+++ flutter/ottugi_curry/lib/view/page/recipe_camera/recipe_camera_page.dart
@@ -1,11 +1,108 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:ottugi_curry/view/comm/default_layout_widget.dart';
+import 'package:ottugi_curry/view/page/list/recipe_recs_list.dart';
 
 class RecipeCameraPage extends StatelessWidget {
   const RecipeCameraPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DefaultLayoutWidget(body: Text('RecipeCameraPage'));
+    return const DefaultLayoutWidget(
+        appBarTitle: '촬영 결과 확인', body: ResultCheck());
   }
+}
+
+class ResultCheck extends StatefulWidget {
+  const ResultCheck({super.key});
+
+  @override
+  State<ResultCheck> createState() => _ResultCheckState();
+}
+
+class _ResultCheckState extends State<ResultCheck> {
+  bool _isCheckTuna = false;
+  bool _isCheckRice = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+            flex: 5,
+            child: Image.asset(
+              'assets/images/tuna.png',
+              fit: BoxFit.fitHeight,
+            )),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    barrierDismissible: true,
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                          child: Center(
+                              child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          checkList('참치', _isCheckTuna, (value) {
+                            setState(() {
+                              _isCheckTuna = value!;
+                            });
+                          }),
+                          checkList('밥', _isCheckRice, (value) {
+                            setState(() {
+                              _isCheckRice = value!;
+                            });
+                          }),
+                        ],
+                      )));
+                    });
+              },
+              child: const Text('수정'),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RecipeRecs()));
+              },
+              child: const Text('완료'),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
+
+void init() {
+  // 인자로 재료들이 들어오면
+  // check 여부를 저장하고 전달할 변수 생성
+}
+
+Row checkList(String text, bool isCheck, ValueChanged<bool?> onChanged) {
+  return Row(
+    children: [
+      Checkbox(
+        value: isCheck,
+        onChanged: onChanged,
+      ),
+      Text(text)
+    ],
+  );
 }
