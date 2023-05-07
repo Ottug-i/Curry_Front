2b430381a7dae5bf2dee4a1f733efad865734aab
Choi Yena <71956482+YenaChoi00@users.noreply.github.com>
2023-05-07 12:07:01 +0900

    Implement Tooltip in Main Page

--- flutter/ottugi_curry/lib/view/page/main/main_page.dart
+++ flutter/ottugi_curry/lib/view/page/main/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ottugi_curry/config/color_schemes.dart';
+import 'package:just_the_tooltip/just_the_tooltip.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,7 +11,23 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final bookTooltip = JustTheController();
+  final humanTooltip = JustTheController();
+  final ladleTooltip = JustTheController();
+
   bool isCliked = false;
+
+  void showBookTooltip() {
+    bookTooltip.showTooltip();
+  }
+
+  void showHumanTooltip() {
+    humanTooltip.showTooltip();
+  }
+
+  void showLadleTooltip() {
+    ladleTooltip.showTooltip();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,38 +52,65 @@ class _MainPageState extends State<MainPage> {
                     : Image.asset('assets/images/light.png')),
             Container(
               alignment: const Alignment(-1.0, 0.0),
-              child: GestureDetector(
-                onTap: () {
-                  Get.toNamed('/recipe');
-                },
-                child: Image.asset(
-                  'assets/images/ladle.png',
-                  fit: BoxFit.none,
+              child: JustTheTooltip(
+                controller: ladleTooltip,
+                content: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    '레시피 추천 받기',
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Get.toNamed('/recipe');
+                  },
+                  child: Image.asset(
+                    'assets/images/ladle.png',
+                    fit: BoxFit.none,
+                  ),
                 ),
               ),
             ),
-            //textBox('레시피 추천받기', -1.0, 1.0),
             Container(
               alignment: const Alignment(-0.1, 0.0),
-              child: GestureDetector(
-                onTap: () {
-                  Get.toNamed('/user');
-                },
-                child: Image.asset(
-                  'assets/images/human.png',
-                  fit: BoxFit.none,
+              child: JustTheTooltip(
+                controller: humanTooltip,
+                preferredDirection: AxisDirection.up,
+                content: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    '마이페이지로 이동',
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Get.toNamed('/user');
+                  },
+                  child: Image.asset(
+                    'assets/images/human.png',
+                    fit: BoxFit.none,
+                  ),
                 ),
               ),
             ),
             Container(
               alignment: const Alignment(0.6, 0.1),
-              child: GestureDetector(
-                onTap: () {
-                  Get.toNamed('/bookmark');
-                },
-                child: Image.asset(
-                  'assets/images/book.png',
-                  fit: BoxFit.none,
+              child: JustTheTooltip(
+                controller: bookTooltip,
+                content: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    '레시피 북마크 조회',
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Get.toNamed('/bookmark');
+                  },
+                  child: Image.asset(
+                    'assets/images/book.png',
+                    fit: BoxFit.none,
+                  ),
                 ),
               ),
             ),
@@ -82,6 +126,11 @@ class _MainPageState extends State<MainPage> {
                     debugPrint('onpressed 눌림');
                     setState(() {
                       isCliked = !isCliked;
+                      if (isCliked == true) {
+                        showBookTooltip();
+                        showHumanTooltip();
+                        showLadleTooltip();
+                      }
                     });
                   },
                 )),
