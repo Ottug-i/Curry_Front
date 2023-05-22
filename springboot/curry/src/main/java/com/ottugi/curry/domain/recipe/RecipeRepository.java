3d4aaa97a684e0e41f1c6728368eecbb997e5f4f
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-05-22 14:21:40 +0900

    Delete Back Code

--- springboot/curry/src/main/java/com/ottugi/curry/domain/recipe/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ottugi.curry.domain.recipe;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findByIdIn(List<Long> recipeId);
-}
